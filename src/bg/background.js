d7944baefec718b554abbff20415af311c60156d
Umar Taufiqulhakim <umartaufiqul@gmail.com>
2020-10-28 23:35:21 +0700

    Added both the UI and the back script for comparison

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,27 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+//Keep track of the current tab that is active
+// var current_tab = null
+// chrome.tabs.onActivated.addListener(tab => {
+//     current_tab = tab
+// })
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+//Listen to the message that is sent from the foreground
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //If the modal button was clicked, open the comparison modal
+    if (request.message === "The button was clicked") {
+      console.log("Hello?")
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var current_tab = tabs[0]
+        chrome.tabs.get(current_tab.id, current_tab_info => {
+            if (!(/^chrome/.test(current_tab_info.url))) {
+                chrome.tabs.insertCSS(null, {file: "css/extModal.css"})
+                chrome.tabs.executeScript(null, {file: 'src/scripts/launchModal.js'}, _ => {
+                    console.log("I injected")
+                })
+            }
+        })
+      })
+    }
+})
---
+++ src/scripts/launchModal.js
@@ -0,0 +1,24 @@
+renderSurveyModal()
+
+function renderSurveyModal() {
+    const modal = document.createElement('div')
+    modal.classList.add('ext-modal')
+  
+    //Construct the modal
+    const article_1 = "<div class='article'> <img src='https://media.heartlandtv.com/images/Biden+and+Trump+election+.jpg' class='article-img'/><h3 class='article-title'> Debate Night For Trump and Biden In Final Campaign Faceoff </h3> <h4 class='article-tag'> <span> Politics </span> -- <span class='ext-sent-mild'> Mild </span> </h4> </div>"
+    const article_2 = "<div class='article'> <img src='https://c.o0bc.com/wp-content/uploads/2020/10/Election_2020_Debate_30924-850x478$large.jpg' class='article-img'/><h3 class='article-title'> More than 15,000 readers said this candidate won the final presidential debate and how it affected their vote </h3> <h4 class='article-tag'> <span> Politics </span> -- <span class='ext-sent-spicy'> Spicy </span> </h4> </div>"
+    const combine_article = "<div style='display: flex; padding: 8px 0px;'>" + article_1+article_2+"</div>"
+    const question_section = "<div style='margin: 0px 2rem;'><h2> Do they share the same opinion? </h2> <button style='background-color: #35CD96;' class='my-button'> Yes </button><button style='background-color: #FF4975' class='my-button'> No </button></div>"
+    const complete_modal = "<div style='margin: 16px 16px;'><h1 style='margin: 2rem 2rem;'> Here are two articles you've recently read: </h1>" + combine_article + question_section + "</div>"
+
+
+    const child = document.createElement('div')
+    child.classList.add('ext-child')
+    child.innerHTML = "<div style='overflow: auto;'>"+complete_modal + "</div>"
+    // child.innerHTML = "<h1> Tes </h1>"
+
+    modal.appendChild(child)
+    document.querySelector('body').appendChild(modal)
+    const parent = document.querySelector('body')
+    parent.insertBefore(modal, parent.firstChild)
+}
