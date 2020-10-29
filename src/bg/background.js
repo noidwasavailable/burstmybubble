8439acef87a7a02c3857fbf6705be242680b9bd6
noidwasavailable <choi9601@gmail.com>
2020-10-29 23:10:59 +0900

    changed prettier to force semicolon

--- src/bg/background.js
+++ src/bg/background.js
@@ -14,21 +14,21 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   //If the modal button was clicked, open the comparison modal
   if (request.message === 'The button was clicked') {
-    console.log('Hello?')
+    console.log('Hello?');
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var current_tab = tabs[0]
+      var current_tab = tabs[0];
       chrome.tabs.get(current_tab.id, (current_tab_info) => {
         if (!/^chrome/.test(current_tab_info.url)) {
-          chrome.tabs.insertCSS(null, { file: 'css/extModal.css' })
+          chrome.tabs.insertCSS(null, { file: 'css/extModal.css' });
           chrome.tabs.executeScript(
             null,
             { file: 'src/scripts/launchModal.js' },
             (_) => {
-              console.log('I injected')
+              console.log('I injected');
             }
-          )
+          );
         }
-      })
-    })
+      });
+    });
   }
-})
+});
--- src/scripts/mainPopup.js
+++ src/scripts/mainPopup.js
@@ -1,5 +1,9 @@
 //Detect the button click on the main popup
-document.getElementById('test-modal').onclick = function(){
-    console.log("Tes")
-    chrome.runtime.sendMessage({message: "The button was clicked"})
-}
+document.getElementById('test-modal').onclick = function () {
+  console.log('Tes');
+  chrome.runtime.sendMessage({ message: 'The button was clicked' });
+};
+
+const openDashboard = () => {
+  console.log('Hello');
+};
