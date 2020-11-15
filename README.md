9a4979ac7392805e8e4d51ce29def47bd9591c61
Umar Taufiqulhakim <umartaufiqul@gmail.com>
2020-11-15 21:15:39 +0700

    Add test call to the server and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Burst My Bubble
+
+An extension to help burst filter bubble when reading various article
+
+## Info
+
+Server IP: 13.125.255.130
--- src/bg/background.js
+++ src/bg/background.js
@@ -26,9 +26,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             null,
             { file: 'src/scripts/launchModal.js' },
             (_) => {
-              // console.log(current_tab_info.url)
-              chrome.tabs.sendMessage(current_tab.id, {url: current_tab_info.url}, function(resp) {
-                console.log(success)
+              const req_json = {
+                url: current_tab_info.url
+              }
+              const param_det = {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(req_json),
+                method: "POST",
+              }
+              fetch('http://13.125.255.130/analyze-entity', param_det).then(
+                  data => {return data.json()}
+              ).then(res => {
+                  console.log(res)
               })
               // console.log('I injected');
             }
