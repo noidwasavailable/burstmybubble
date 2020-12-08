129143d77340c1e32b960a6219064391036c68a1
noidwasavailable <choi9601@gmail.com>
2020-12-08 17:43:44 +0900

    updated history to include only urls on firestore

--- src/bg/background.js
+++ src/bg/background.js
@@ -14,18 +14,58 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-var db = firebase.firestore();
-
+const db = firebase.firestore();
 
 chrome.browserAction.onClicked.addListener(() => {
-  console.log('Dashboard please');
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var url = tabs[0].url;
-    var history = localStorage.getItem('history');
-    var history = history ? JSON.parse(history) : [];
-    if (!history.includes(url)) {
+    let url = tabs[0].url;
+
+    //get valid urls from localStorage
+    let valid_urls = localStorage.getItem('valid_urls');
+    //if it does not exist, store a list of valid urls that are on the server
+
+    if (valid_urls) {
+      console.log('valid_urls exists. Updating...');
+      valid_urls = JSON.parse(valid_urls);
+      db.collection('Articles')
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach((doc) => {
+            const newUrl = doc.data().url;
+            if (!valid_urls.includes(newUrl)) {
+              valid_urls.push(doc.data().url);
+            }
+          });
+        })
+        .then(() => {
+          localStorage.setItem('valid_urls', JSON.stringify(valid_urls));
+        });
+    } else {
+      console.log('valid_urls does not exist. Creating...');
+      valid_urls = [];
+      db.collection('Articles')
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach((doc) => {
+            const newUrl = doc.data().url;
+            valid_urls.push(newUrl);
+          });
+        })
+        .then(() => {
+          localStorage.setItem('valid_urls', JSON.stringify(valid_urls));
+        });
+    }
+    console.log('Setting valid_urls');
+
+    //get history from localStorage
+    let history = localStorage.getItem('history');
+    //if it does not exist, create an empty one
+    history = history ? JSON.parse(history) : [];
+    //if the current url is not on there, add it to the history
+    if (!history.includes(url) && valid_urls.includes(url)) {
       history.push(url);
     }
+    console.log('Setting history');
     localStorage.setItem('history', JSON.stringify(history));
   });
   chrome.tabs.create({ url: 'src/dashboard/dashboard_home.html' });
